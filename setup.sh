291e0c252da401dd2662027d21cffe629a920a8d
Karl Krauth <karl.krauth@gmail.com>
2020-03-08 23:38:30 -0700

    Update setup to install fish

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
-#!/bin/bash
+#/bin/bash
+
+# Install fish.
+sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt-get update
+sudo apt-get install fish
+echo fish >> ~/.bashrc
 
 # Copy tmux files.
 cp .tmux.conf ~/.tmux.conf
