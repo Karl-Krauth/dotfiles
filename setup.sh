2c0f7ca2e5ee04c502cd01daa0bd02d9f0428223
Karl Krauth <karl.krauth@gmail.com>
2020-03-09 00:03:08 -0700

    Install anaconda3

--- setup.sh
+++ setup.sh
@@ -4,7 +4,13 @@
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt-get update
 sudo apt-get install fish
-echo fish >> ~/.bashrc
+echo fish > ~/.bashrc
+cp config.fish ~/.config/fish/config.fish
+
+# Install anaconda3
+mkdir ~/bin
+wget https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
+bash Anaconda3-2019.10-Linux-x86_64.sh -b -p ~/bin/anaconda3
 
 # Copy tmux files.
 cp .tmux.conf ~/.tmux.conf
