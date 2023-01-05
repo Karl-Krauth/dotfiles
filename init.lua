88c1bad8539230bc79c3b4b3c9d2d97f6b425b00
Karl Krauth <karl.krauth@gmail.com>
2023-01-05 11:06:30 +1100

    Add subterminal capabilities to vim.

--- init.lua
+++ init.lua
@@ -6,6 +6,25 @@ local opt = vim.opt  -- to set options
 
 require("plugins")
 
+-- Subterminal configuration.
+require("toggleterm").setup{
+  size = 20,
+  open_mapping = [[<c-\>]],
+  close_mapping = [[<c-\>]],
+  hide_numbers = true,
+  autochdir = false,
+  shade_terminals = true,
+  start_in_insert = true,
+  insert_mappings = true,
+  terminal_mappings = true,
+  persist_size = true,
+  persist_mode = true,
+  direction = 'horizontal',
+  close_on_exit = true,
+  shell = vim.o.shell,
+  auto_scroll = true,
+}
+
 -- Display more line info in vim.
 opt.number = true
 opt.relativenumber = true
@@ -19,7 +38,6 @@ opt.shiftwidth = 4
 opt.smartindent = true
 
 -- Color settings.
-opt.termguicolors = true
 cmd.colorscheme("catppuccin-macchiato")
 
 -- Show trailing whitespaces and tabs.
--- plugins.lua
+++ plugins.lua
@@ -1,4 +1,10 @@
 return require('packer').startup(function(use)
     use("wbthomason/packer.nvim")
     use{"catppuccin/nvim", as="catppuccin"}
+    use("github/copilot.vim")
+    use{
+        "akinsho/toggleterm.nvim",
+        tag='*',
+        config=function() require("toggleterm").setup() end
+     }
 end)
