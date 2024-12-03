d7af1e362c5abbe9370eeba573a705bc0a05684a
Karl Krauth <karl.krauth@gmail.com>
2024-12-03 15:43:06 -0800

    More language servers.

--- init.lua
+++ init.lua
@@ -139,7 +139,11 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'pyright'},
+  ensure_installed = {
+      'clangd',
+      'pyright',
+      'ruff',
+  },
   handlers = {
     function(server_name)
       require('lspconfig')[server_name].setup({})
