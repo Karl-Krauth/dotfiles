5e2a3679c7885e00db412b5d64d3a1adc7a2f8c0
Karl Krauth <karl.krauth@gmail.com>
2025-11-27 09:19:38 -0800

    Add supermaven and remove copilot.

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,49 +1,41 @@
 -- Define aliases.
-local cmd = vim.cmd  -- to execute Vim commands e.g. cmd('pwd')
-local fn = vim.fn    -- to call Vim functions e.g. fn.bufnr()
-local g = vim.g      -- a table to access global variables
-local opt = vim.opt  -- to set options
-
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-opt.rtp:prepend(lazypath)
-
--- Make sure clipboard uses the system clipboard.
-opt.clipboard = "unnamedplus"
-if fn.has("wsl") == 1 then
-  g.clipboard = {
-    name = "WslClipboard",
-    copy = {
-      ["+"] = "clip.exe",
-      ["*"] = "clip.exe",
-    },
-    paste = {
-      ["+"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-      ["*"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    },
-    cache_enabled = 0,
-  }
-end
+local cmd = vim.cmd
+local fn = vim.fn
+local g = vim.g
+local opt = vim.opt
+local keymap = vim.keymap
 
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+g.mapleader = " "
+g.maplocalleader = "\\"
 
--- Setup lazy.nvim
+-- Bootstrap lazy.nvim.
+local lazypath = fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "--branch=stable",
+        lazyrepo,
+        lazypath
+    })
+
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            {"Failed to clone lazy.nvim:\n", "ErrorMsg"},
+            {out, "WarningMsg"},
+            {"\nPress any key to exit..."},
+        }, true, {})
+        fn.getchar()
+        os.exit(1)
+    end
+end
+opt.rtp:prepend(lazypath)
+
+-- Setup lazy.nvim.
 require("lazy").setup({
   spec = {
     {
@@ -52,31 +44,25 @@ require("lazy").setup({
         version = "1.*",
         opts = {},
     },
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/nvim-cmp'},
+    {"hrsh7th/cmp-nvim-lsp"},
+    {"hrsh7th/nvim-cmp"},
     {"williamboman/mason.nvim", commit = "4da89f3"},
     {"williamboman/mason-lspconfig.nvim", commit = "1a31f82"},
-    {'neovim/nvim-lspconfig'},
-    {'zbirenbaum/copilot.lua'},
-    {
-        "zbirenbaum/copilot-cmp",
-        config = function ()
-            require("copilot_cmp").setup()
-        end
-    },
+    {"neovim/nvim-lspconfig"},
+    {"supermaven-inc/supermaven-nvim"},
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate"
     },
     {
-        'akinsho/toggleterm.nvim',
+        "akinsho/toggleterm.nvim",
         version = "*",
         config = true
     },
     {
-        'nvim-telescope/telescope.nvim',
-        branch = '0.1.x',
-        dependencies = { 'nvim-lua/plenary.nvim' }
+        "nvim-telescope/telescope.nvim",
+        branch = "0.1.x",
+        dependencies = { "nvim-lua/plenary.nvim" }
     },
     {
         "catppuccin/nvim",
@@ -84,9 +70,7 @@ require("lazy").setup({
         priority = 1000
     },
   },
-  -- colorscheme that will be used when installing plugins.
   install = {colorscheme = {"habamax"}},
-  -- automatically check for plugin updates
   checker = {enabled = true},
 })
 
@@ -103,14 +87,14 @@ require("toggleterm").setup{
   terminal_mappings = true,
   persist_size = true,
   persist_mode = true,
-  direction = 'horizontal',
+  direction = "horizontal",
   close_on_exit = true,
   shell = vim.o.shell,
   auto_scroll = true,
 }
 
 -- Syntax highlighting.
-require'nvim-treesitter.configs'.setup{
+require("nvim-treesitter.configs").setup{
   ensure_installed = "all",
   sync_install = false,
   auto_install = true,
@@ -123,54 +107,46 @@ require'nvim-treesitter.configs'.setup{
   },
 }
 
--- Code completion and navigation.
-require("copilot").setup({
-  suggestion = { enabled = false },
-  panel = { enabled = false },
+-- Reserve a space in the gutter.
+opt.signcolumn = "yes"
+
+-- Add cmp_nvim_lsp capabilities settings to lspconfig before configuring language servers.
+vim.lsp.config("*", {
+  capabilities = require("cmp_nvim_lsp").default_capabilities(),
 })
 
--- Reserve a space in the gutter
-vim.opt.signcolumn = 'yes'
-
--- Add cmp_nvim_lsp capabilities settings to lspconfig
--- This should be executed before you configure any language server
-vim.lsp.config('*', {
-  capabilities = require('cmp_nvim_lsp').default_capabilities(),
-})
-
--- This is where you enable features that only work
--- if there is a language server active in the file
-vim.api.nvim_create_autocmd('LspAttach', {
-  desc = 'LSP actions',
+-- Commands that get defined when an LSP is attached.
+vim.api.nvim_create_autocmd("LspAttach", {
+  desc = "LSP actions",
   callback = function(event)
     local opts = {buffer = event.buf}
 
-    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-    vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+    keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+    keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+    keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+    keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+    keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+    keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+    keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+    keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+    keymap.set({"n", "x"}, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+    keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
   end,
 })
 
-require('mason').setup({})
-require('mason-lspconfig').setup({
+-- LSP installer.
+require("mason").setup({})
+require("mason-lspconfig").setup({
   ensure_installed = {
-      'clangd',
-      'pyright',
-      'ruff',
-      'tinymist',
-      'vtsls',
+      "clangd",
+      "pyright",
+      "ruff",
+      "tinymist",
+      "vtsls",
   },
 })
 
--- require('lspconfig').rust_analyzer.setup({})
-
+-- Typst LSP config.
 vim.lsp.config("tinymist", {
                 formatterMode = "typstyle",
                 exportPdf = "onType",
@@ -178,6 +154,7 @@ vim.lsp.config("tinymist", {
         }
 )
 
+-- Automatically open the typst previewer when opening a typst file.
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "typst",
   callback = function(args)
@@ -188,46 +165,49 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
-local cmp = require('cmp')
-
-local has_words_before = function()
-  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
-end
-
-cmp.setup({
-  sources = {
-    {name = 'nvim_lsp'},
-    {name = "copilot"},
-  },
-  snippet = {
-    expand = function(args)
-      vim.snippet.expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<CR>'] = cmp.mapping.confirm({select = true}),
-    ["<Tab>"] = vim.schedule_wrap(function(fallback)
-      if cmp.visible() and has_words_before() then
-        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-      else
-        fallback()
-      end
-    end),
-  }),
+-- Configure AI code completion to integrate with cmp.
+require("supermaven-nvim").setup({
+    keymaps = {
+        accept_suggestion = "",
+        clear_suggestion = "",
+        accept_word = ""
+    },
+    disable_inline_completion = true
 })
 
--- Go to center of page after page up/down.
-vim.keymap.set("n", "<C-d>", "<C-d>zz", {noremap = true})
-vim.keymap.set("n", "<C-u>", "<C-u>zz", {noremap = true})
+local cmp = require("cmp")
+cmp.setup({
+    sources = {
+        {name = "supermaven"},
+        {name = "nvim_lsp"},
+    },
+    snippet = {
+        expand = function(args)
+            vim.snippet.expand(args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({
+        ["<CR>"] = cmp.mapping.confirm({select = true}),
+        ["<Tab>"] = vim.schedule_wrap(function(fallback)
+            if cmp.visible() and has_words_before() then
+                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+            else
+                fallback()
+            end
+        end),
+    }),
+})
 
 -- File navigation.
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+local builtin = require("telescope.builtin")
+keymap.set("n", "<leader>ff", builtin.find_files, {})
+keymap.set("n", "<leader>fg", builtin.live_grep, {})
+keymap.set("n", "<leader>fb", builtin.buffers, {})
+keymap.set("n", "<leader>fh", builtin.help_tags, {})
+
+-- Go to center of page after page up/down.
+keymap.set("n", "<C-d>", "<C-d>zz", {noremap = true})
+keymap.set("n", "<C-u>", "<C-u>zz", {noremap = true})
 
 -- Display more line info in vim.
 opt.number = true
@@ -253,3 +233,20 @@ cmd.colorscheme("catppuccin-macchiato")
 -- Show trailing whitespaces and tabs.
 cmd("highlight unwanted_characters ctermbg=red guibg=red")
 cmd("match unwanted_characters /\\s\\+$\\|\\t/")
+
+-- Make sure clipboard uses the system clipboard.
+opt.clipboard = "unnamedplus"
+if fn.has("wsl") == 1 then
+    g.clipboard = {
+        name = "WslClipboard",
+        copy = {
+            ["+"] = "clip.exe",
+            ["*"] = "clip.exe",
+        },
+        paste = {
+            ["+"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+            ["*"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+  }
+end
