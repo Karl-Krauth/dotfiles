c96dfc90e8a91bb5e2d408e0fd24c39204e163e7
Karl Krauth <karl.krauth@gmail.com>
2025-11-27 11:41:03 -0800

    Simplify init.lua and make indentation consistent.

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -12,14 +12,13 @@ g.maplocalleader = "\\"
 
 -- Bootstrap lazy.nvim.
 local lazypath = fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+if not vim.uv.fs_stat(lazypath) then
     local out = fn.system({
         "git",
         "clone",
         "--filter=blob:none",
         "--branch=stable",
-        lazyrepo,
+        "https://github.com/folke/lazy.nvim.git",
         lazypath
     })
 
@@ -37,132 +36,122 @@ opt.rtp:prepend(lazypath)
 
 -- Setup lazy.nvim.
 require("lazy").setup({
-  spec = {
-    {
-        "chomosuke/typst-preview.nvim",
-        ft = "typst",
-        version = "1.*",
-        opts = {},
+    spec = {
+        {
+            "akinsho/toggleterm.nvim",
+            config = true,
+            version = "*",
+            opts = {
+                open_mapping = [[<leader>t]],
+                hide_numbers = true,
+                autochdir = true,
+                insert_mappings = false,
+                direction = "horizontal",
+            }
+        },
+        {
+            "catppuccin/nvim",
+            name = "catppuccin",
+            opts = {
+                auto_integrations = true,
+                flavour = "macchiato",
+            },
+            version = "*",
+        },
+        {
+            "chomosuke/typst-preview.nvim",
+            config = true,
+            ft = "typst",
+            version = "*",
+        },
+        {"hrsh7th/cmp-nvim-lsp"},
+        {"hrsh7th/nvim-cmp"},
+        {"neovim/nvim-lspconfig", version = "*"},
+        {
+            "nvim-telescope/telescope.nvim",
+            dependencies = {"nvim-lua/plenary.nvim"},
+            version = "*",
+        },
+        {
+            "nvim-treesitter/nvim-treesitter",
+            build = ":TSUpdate",
+            lazy = false,
+            version = "*",
+        },
+        {"supermaven-inc/supermaven-nvim"},
+        {"williamboman/mason.nvim", version = "*"},
+        {"williamboman/mason-lspconfig.nvim", version = "*"},
     },
-    {"hrsh7th/cmp-nvim-lsp"},
-    {"hrsh7th/nvim-cmp"},
-    {"williamboman/mason.nvim", commit = "4da89f3"},
-    {"williamboman/mason-lspconfig.nvim", commit = "1a31f82"},
-    {"neovim/nvim-lspconfig"},
-    {"supermaven-inc/supermaven-nvim"},
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate"
-    },
-    {
-        "akinsho/toggleterm.nvim",
-        version = "*",
-        config = true
-    },
-    {
-        "nvim-telescope/telescope.nvim",
-        branch = "0.1.x",
-        dependencies = { "nvim-lua/plenary.nvim" }
-    },
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000
-    },
-  },
-  install = {colorscheme = {"habamax"}},
-  checker = {enabled = true},
+    checker = {enabled = true, frequency = 604800},
 })
 
--- Subterminal configuration.
-require("toggleterm").setup{
-  size = 20,
-  open_mapping = [[<leader>t]],
-  close_mapping = [[<leader>t]],
-  hide_numbers = true,
-  autochdir = false,
-  shade_terminals = true,
-  start_in_insert = true,
-  insert_mappings = false,
-  terminal_mappings = true,
-  persist_size = true,
-  persist_mode = true,
-  direction = "horizontal",
-  close_on_exit = true,
-  shell = vim.o.shell,
-  auto_scroll = true,
-}
+-- Catppuccin colorscheme.
+cmd.colorscheme("catppuccin")
 
 -- Syntax highlighting.
-require("nvim-treesitter.configs").setup{
-  ensure_installed = "all",
-  sync_install = false,
-  auto_install = true,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-  ignore_install = {
-    "ipkg",
-  },
-}
-
--- Reserve a space in the gutter.
-opt.signcolumn = "yes"
+require("nvim-treesitter.configs").setup({
+    ensure_installed = "all",
+    sync_install = false,
+    auto_install = true,
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
+})
 
 -- Add cmp_nvim_lsp capabilities settings to lspconfig before configuring language servers.
 vim.lsp.config("*", {
-  capabilities = require("cmp_nvim_lsp").default_capabilities(),
+    capabilities = require("cmp_nvim_lsp").default_capabilities(),
 })
 
 -- Commands that get defined when an LSP is attached.
 vim.api.nvim_create_autocmd("LspAttach", {
-  desc = "LSP actions",
-  callback = function(event)
-    local opts = {buffer = event.buf}
+    desc = "LSP actions",
+    callback = function(event)
+        local opts = {buffer = event.buf}
 
-    keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
-    keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
-    keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
-    keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
-    keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
-    keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
-    keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
-    keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
-    keymap.set({"n", "x"}, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
-    keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
-  end,
+        keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+        keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+        keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+        keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+        keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+        keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+        keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+        keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+        keymap.set({"n", "x"}, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+        keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+    end,
 })
 
 -- LSP installer.
 require("mason").setup({})
 require("mason-lspconfig").setup({
-  ensure_installed = {
-      "clangd",
-      "pyright",
-      "ruff",
-      "tinymist",
-      "vtsls",
-  },
+    ensure_installed = {
+        "clangd",
+        "pyright",
+        "ruff",
+        "tinymist",
+        "vtsls",
+    },
 })
 
 -- Typst LSP config.
 vim.lsp.config("tinymist", {
-                formatterMode = "typstyle",
-                exportPdf = "onType",
-                semanticTokens = "disable"
-        }
+    formatterMode = "typstyle",
+    exportPdf = "onType",
+    semanticTokens = "disable"
+}
 )
 
 -- Automatically open the typst previewer when opening a typst file.
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "typst",
-  callback = function(args)
-    if vim.bo[args.buf].buftype ~= "" then return end
-    if vim.b[args.buf].typst_preview_started then return end
-    vim.b[args.buf].typst_preview_started = true
-    vim.cmd("TypstPreview")
-  end,
+    pattern = "typst",
+    callback = function(args)
+        if vim.bo[args.buf].buftype ~= "" then return end
+        if vim.b[args.buf].typst_preview_started then return end
+        vim.b[args.buf].typst_preview_started = true
+        vim.cmd("TypstPreview")
+    end,
 })
 
 -- Configure AI code completion to integrate with cmp.
@@ -221,14 +210,8 @@ opt.expandtab = true
 opt.shiftwidth = 4
 opt.smartindent = true
 
--- Color settings.
-require("catppuccin").setup{
-    no_italic = true,
-    integrations = {
-        treesitter = true,
-    },
-}
-cmd.colorscheme("catppuccin-macchiato")
+-- Reserve a space in the gutter.
+opt.signcolumn = "yes"
 
 -- Show trailing whitespaces and tabs.
 cmd("highlight unwanted_characters ctermbg=red guibg=red")
@@ -246,7 +229,7 @@ if fn.has("wsl") == 1 then
         paste = {
             ["+"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
             ["*"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    },
-    cache_enabled = 0,
-  }
+        },
+        cache_enabled = 0,
+    }
 end
